87f19f56339fccc9056060911c23c5319e233af8
Dương Thành Sơn <duongthanhson2004@gmail.com>
2026-06-18 17:29:29 +0700

    fix: remove try-catch blocks in AuthController to allow ErrorHandlingMiddleware to properly handle exceptions

--- Presentation/QuanLyHoaLan.API/Controllers/AuthController.cs
+++ Presentation/QuanLyHoaLan.API/Controllers/AuthController.cs
@@ -13,44 +13,23 @@ public class AuthController : BaseController
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginCommand command)
     {
-        try
-        {
-            var result = await Mediator.Send(command);
-            return OkResult(result);
-        }
-        catch (Exception ex)
-        {
-            return Unauthorized(new { message = ex.Message });
-        }
+        var result = await Mediator.Send(command);
+        return OkResult(result);
     }
 
     [AllowAnonymous]
     [HttpPost("google-login")]
     public async Task<IActionResult> GoogleLogin([FromBody] LoginWithGoogleCommand command)
     {
-        try
-        {
-            var result = await Mediator.Send(command);
-            return OkResult(result);
-        }
-        catch (Exception ex)
-        {
-            return Unauthorized(new { message = ex.Message });
-        }
+        var result = await Mediator.Send(command);
+        return OkResult(result);
     }
 
     [AllowAnonymous]
     [HttpPost("refresh-token")]
     public async Task<IActionResult> RefreshToken([FromBody] QuanLyHoaLan.Application.Features.Auth.Commands.RefreshToken.RefreshTokenCommand command)
     {
-        try
-        {
-            var result = await Mediator.Send(command);
-            return OkResult(result);
-        }
-        catch (Exception ex)
-        {
-            return Unauthorized(new { message = ex.Message });
-        }
+        var result = await Mediator.Send(command);
+        return OkResult(result);
     }
 }
